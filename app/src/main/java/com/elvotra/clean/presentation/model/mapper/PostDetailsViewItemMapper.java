413c49c2f6858c4ccb250e09e09bc30203e2db81
Mostafa <hadian@gmail.com>
2018-04-30 00:14:17 +0100

    Refactoring

--- app/src/main/java/com/elvotra/clean/presentation/model/mapper/PostDetailsViewItemMapper.java
+++ app/src/main/java/com/elvotra/clean/presentation/model/mapper/PostDetailsViewItemMapper.java
@@ -5,6 +5,7 @@ import com.elvotra.clean.domain.model.Post;
 import com.elvotra.clean.domain.model.User;
 import com.elvotra.clean.presentation.model.PostCommentViewItem;
 import com.elvotra.clean.presentation.model.PostDetailsViewItem;
+import com.elvotra.clean.utils.Constants;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +13,6 @@ import java.util.List;
 public class PostDetailsViewItemMapper {
 
     private static PostDetailsViewItemMapper INSTANCE;
-    private static final String AVATAR = "https://api.adorable.io/avatars/285/";
 
     public static PostDetailsViewItemMapper getInstance() {
         if (INSTANCE == null) {
@@ -29,7 +29,7 @@ public class PostDetailsViewItemMapper {
         List<PostCommentViewItem> postCommentViewItems = new ArrayList<>(comments.size());
         for (int i = 0; i < comments.size(); i++) {
             Comment comment = comments.get(i);
-            String avatar = AVATAR + ((comment.getEmail() != null) ? comment.getEmail() : "");
+            String avatar = Constants.AVATAR_BASE_URL + ((comment.getEmail() != null) ? comment.getEmail() : "");
             PostCommentViewItem commentViewItem = new PostCommentViewItem(comment.getId(), comment.getName(), comment.getEmail(), avatar, comment.getBody());
             postCommentViewItems.add(commentViewItem);
         }
@@ -40,7 +40,7 @@ public class PostDetailsViewItemMapper {
     public PostDetailsViewItem transform(Post post, User user, List<PostCommentViewItem> commentViewItems) {
         String username = (user != null) ? user.getName() : "Unknown";
         String useremail = (user != null && user.getEmail() != null) ? user.getEmail() : "";
-        String avatar = AVATAR + ((user != null && user.getEmail() != null) ? user.getEmail() : "");
+        String avatar = Constants.AVATAR_BASE_URL + ((user != null && user.getEmail() != null) ? user.getEmail() : "");
 
         return new PostDetailsViewItem(post.getId(), username, useremail, avatar, post.getTitle(), post.getBody(), commentViewItems);
     }
